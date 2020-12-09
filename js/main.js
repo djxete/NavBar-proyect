f0c1ba294761f1a031833643c067d7f719e63627
Ignacio Herráez <ignacio_hg_87@hotmail.com>
2020-12-09 12:41:28 +0100

    proyect finished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-alert("probando");
+// ======== VARIABLES =====
+
+const btn = document.querySelector(".nav-toggle");
+let links = document.querySelector(".links");
+
+
+
+// ======== EVENT LISTENER AND FUNCTIONS =====
+
+// One way to do it
+
+function toggle() {
+
+    btn.addEventListener("click", () => {
+        links.classList.toggle("show-links");
+    })
+}
+
+toggle();
+
+
+// Another way to do it
+
+// btn.addEventListener("click", () => {
+
+//     (links.classList.contains("show-links"))
+//         ? links.classList.remove("show-links")
+//         : links.classList.add("show-links");
+
+// })
+
+
+
